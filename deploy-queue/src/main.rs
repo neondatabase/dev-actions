a365bbe19fa8d2427420bd16ffc5338403a47d75
Urša Gorše <61006117+Shugenya@users.noreply.github.com>
2025-09-15 19:46:38 +0200

    Update deploy-queue/src/main.rs
    Co-authored-by: JC Grünhage <jc@neon.tech>

--- deploy-queue/src/main.rs
+++ deploy-queue/src/main.rs
@@ -71,7 +71,7 @@ const BUSY_RETRY: Duration = Duration::from_secs(5);
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let log_env = env_logger::Env::default().filter_or("MUTEXBOT_LOG_LEVEL", "info");
+    let log_env = env_logger::Env::default().filter_or("DEPLOY_QUEUE_LOG_LEVEL", "info");
     env_logger::Builder::from_env(log_env).init();
     let args = cli::Cli::parse();
 
