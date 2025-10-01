87ebd104d428dc80772f18c659aaa4a2bf62522b
ursa gorse <ursa@neon.tech>
2025-10-01 09:42:05 +0200

    Fixing PR comment about thinner wrapper.

--- deploy-queue/src/main.rs
+++ deploy-queue/src/main.rs
@@ -1,12 +1,4 @@
-use anyhow::Result;
-use clap::Parser;
-use deploy_queue::{cli, run_deploy_queue};
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    let log_env = env_logger::Env::default().filter_or("DEPLOY_QUEUE_LOG_LEVEL", "info");
-    env_logger::Builder::from_env(log_env).init();
-    let args = cli::Cli::parse();
-
-    run_deploy_queue(args.mode).await
+	deploy_queue::main().await
 }
