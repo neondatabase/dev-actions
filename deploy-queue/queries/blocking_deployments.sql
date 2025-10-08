fef69d8d1cea6858db5e1b693a4b455358d521c8
Jan Christian Grünhage <jc@neon.tech>
2025-10-09 01:34:52 +0200

    chore(deploy-queue): use descriptive names for blocking_deployments query

--- deploy-queue/queries/blocking_deployments.sql
+++ deploy-queue/queries/blocking_deployments.sql
@@ -10,19 +10,19 @@
 -- Parameters:
 -- $1: deployment_id - The ID of the deployment to check for blockers
 SELECT
-    d2.id,
-    d2.region,
-    d2.environment,
-    d2.component,
-    d2.version,
-    d2.url,
-    d2.note,
-    d2.start_timestamp,
-    d2.finish_timestamp,
-    d2.cancellation_timestamp,
-    d2.cancellation_note,
-    d2.concurrency_key,
-    e.buffer_time
+    blocking.id,
+    blocking.region,
+    blocking.environment,
+    blocking.component,
+    blocking.version,
+    blocking.url,
+    blocking.note,
+    blocking.start_timestamp,
+    blocking.finish_timestamp,
+    blocking.cancellation_timestamp,
+    blocking.cancellation_note,
+    blocking.concurrency_key,
+    environments.buffer_time
 FROM
     (
         SELECT
@@ -31,21 +31,21 @@ FROM
             deployments
         WHERE
             id = $1
-    ) d1
-    JOIN environments e ON d1.environment = e.environment
-    JOIN deployments d2 ON (
-        d1.region = d2.region
+    ) self
+    JOIN environments ON self.environment = environments.environment
+    JOIN deployments blocking ON (
+        self.region = blocking.region
         AND (
-            d1.concurrency_key IS NULL
-            OR d2.concurrency_key IS NULL
-            OR d1.concurrency_key != d2.concurrency_key
+            self.concurrency_key IS NULL
+            OR blocking.concurrency_key IS NULL
+            OR self.concurrency_key != blocking.concurrency_key
         )
-        AND d2.id < d1.id
+        AND blocking.id < self.id
         AND (
-            d2.finish_timestamp IS NULL
-            OR d2.finish_timestamp > NOW() - INTERVAL '1 minute' * e.buffer_time
+            blocking.finish_timestamp IS NULL
+            OR blocking.finish_timestamp > NOW() - INTERVAL '1 minute' * environments.buffer_time
         )
-        AND d2.cancellation_timestamp IS NULL
+        AND blocking.cancellation_timestamp IS NULL
     )
 ORDER BY
-    d2.id ASC
+    blocking.id ASC
